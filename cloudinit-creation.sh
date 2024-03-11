64880dfc37d2c3e0bd514a9995e8a0ca15424841
ryphon <dylan@shepard.dev>
2024-03-11 17:54:37 -0600

    k?

--- cloudinit-creation.sh
+++ cloudinit-creation.sh
@@ -12,12 +12,12 @@ sudo virt-customize -a $IMAGE --run-command 'echo "ubuntu  ALL=(ALL) NOPASSWD:AL
 sudo sudo virt-sysprep -a $IMAGE --operations machine-id
 scp $IMAGE root@$PROXMOX:~/
 ssh -t root@$PROXMOX "~/cloudinit2.sh"
-#sudo qm create 9000 --name "noble-cloudinit-template" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
-#sudo qm importdisk 9000 $IMAGE vm2
-#sudo qm set 9000 --scsihw virtio-scsi-pci --scsi0 vm2:vm-9000-disk-0
-#sudo qm set 9000 --boot c --bootdisk scsi0
-#sudo qm set 9000 --ide2 vm2:cloudinit
-#sudo qm set 9000 --serial0 socket --vga serial0
-#sudo qm set 9000 --agent enabled=1
-#sudo qm template 9000
+#sudo qm create 9001 --name "noble-cloudinit-template" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
+#sudo qm importdisk 9001 $IMAGE vm2
+#sudo qm set 9001 --scsihw virtio-scsi-pci --scsi0 vm2:vm-9001-disk-0
+#sudo qm set 9001 --boot c --bootdisk scsi0
+#sudo qm set 9001 --ide2 vm2:cloudinit
+#sudo qm set 9001 --serial0 socket --vga serial0
+#sudo qm set 9001 --agent enabled=1
+#sudo qm template 9001
 #rm $IMAGE
